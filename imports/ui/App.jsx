450e54e7baafff297080f7775aed08ffe3d57160
Cbodtorf <cbodtorf@gmail.com>
2016-09-13 16:10:01 -0400

    removed autopublish for security, tasks.js is publishing 'tasks', and App.jsx is subscribing to it. Also adding private/public tasks to be toggled, so now we have Selective publishing. Added some basic user id checks in tasks.js for deleting and toggling checking.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -56,9 +56,18 @@ class App extends Component {
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked)
     }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser && this.props.currentUser._id
+      const showPrivateButton = task.owner === currentUserId
+
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+          />
+      );
+    });
   }
 
   render() {
@@ -109,6 +118,8 @@ App.propTypes = {
 // createContainer wraps App component and gives it access
 // to Meteor Data Collection.
 export default createContainer(() => {
+  Meteor.subscribe('tasks');
+
   return {
     tasks: Tasks.find({}, {sort: {createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
-
-import { Tasks } from '../api/tasks.js'
+import { Meteor } from 'meteor/meteor'
+import classnames from 'classnames'
 
 // Task Component -- represents a single todo item
 export default class Task extends Component {
@@ -19,15 +19,28 @@ export default class Task extends Component {
     /******************
     * Uses Meteors call method to separate database concerns to server. (imports/ui/tasks.js called in server/main.js)
     * @param (str) method defined in server for removing tasks
-    * @param (int) indentifies a subset of collection to remove
+    * @param ({}._id) indentifies a subset of collection to remove
     *******************/
     Meteor.call('tasks.remove', this.props.task._id)
+  }
+
+  togglePrivate() {
+    /******************
+    * Uses Meteors call method to separate database concerns to server. (imports/ui/tasks.js called in server/main.js)
+    * @param (str) method defined in server for removing tasks
+    * @param ({}._id) indentifies a subset of collection to update
+    * @param (obj) calls $set to toggle current value
+    *******************/
+    Meteor.call('tasks.setPrivate', this.props.task._id, !this.props.task.private)
   }
 
   render() {
     // Give tasks a different className when they are checked off,
     // so that we can style them differently.
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
 
 
     return (
@@ -43,6 +56,12 @@ export default class Task extends Component {
           onClick={this.toggleChecked.bind(this)}
         />
 
+      { this.props.showPrivateButton ? (
+        <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+          { this.props.task.private ? 'Private' : 'Public' }
+        </button>
+      ) : ''}
+
       <span className="text">
         <strong>{this.props.task.username}</strong>: {this.props.task.text}
       </span>
@@ -50,3 +69,10 @@ export default class Task extends Component {
     );
   }
 }
+
+Task.propTypes = {
+   // This component gets the task to display through a React prop.
+   // We can use propTypes to indicate it is required
+   task: PropTypes.object.isRequired,
+   showPrivateButton: React.PropTypes.bool.isRequired,
+ };
